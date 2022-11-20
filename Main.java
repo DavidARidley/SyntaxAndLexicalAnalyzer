5d69c01b427850acca00896b6f68ac88cea9709d
DavidARidley <118642356+DavidARidley@users.noreply.github.com>
2022-11-19 21:58:12 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,181 @@
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+	public static HashMap<String, Integer> tokenValues = new HashMap<>();
+	
+	public static void main(String[] args) {
+		//Load our token values
+		loadTokenValues();
+		//Get Code File
+		//Path path = Path.of("test.txt");
+		//Path path = Path.of("test2.txt");
+		Path path = Path.of("lexerr.txt");
+		//Path path = Path.of("synerr.txt");
+		
+		//Convert into Lists containing number codes and another the valid lexemes
+		List<String> lexemes = getLexemes(path);
+		List<Integer> tokens = Lexer.parseTokens(lexemes);
+		
+		//Check lists for valid syntax
+		RDA syn = new RDA(lexemes, tokens);
+		syn.program();
+	}
+	
+	public static void loadTokenValues() {
+		//Loads all the lexemes into a hashmap
+		tokenValues.put("BEGIN", 0);
+		tokenValues.put("END", 1);
+		tokenValues.put("when", 2);
+		tokenValues.put("elsewhen", 22);
+		tokenValues.put("loop", 3);
+		tokenValues.put("+", 4);
+		tokenValues.put("-", 5);
+		tokenValues.put("*", 6);
+		tokenValues.put("/", 7);
+		tokenValues.put("=", 8);
+		tokenValues.put(">", 9);
+		tokenValues.put("<", 10);
+		tokenValues.put(">=", 11);
+		tokenValues.put("<=", 12);
+		tokenValues.put("==", 13);
+		tokenValues.put("!=", 14);
+		tokenValues.put("!", 15);
+		tokenValues.put("num", 16);
+		tokenValues.put("{", 20);
+		tokenValues.put("}", 21);
+		tokenValues.put("(", 23);
+		tokenValues.put(")", 24);
+		tokenValues.put("int_one_byte", 25);
+		tokenValues.put("id", 26);
+		tokenValues.put("int_two_byte", 27);
+		tokenValues.put("int_four_byte", 28);
+		tokenValues.put("int_eight_byte", 29);
+		tokenValues.put("&", 30);
+		tokenValues.put("|", 31);
+		tokenValues.put("E", 32);
+		tokenValues.put("%", 32);
+		
+	}
+	public static List<String> getLexemes(Path pathToFile) {
+		/*
+		 * Reads each line and separates the lexemes by spaces, storing them in a list.
+		*/
+		List<String> lexemes = new ArrayList();
+		try {
+			List<String> lines = Files.readAllLines(pathToFile);
+			for(String line: lines) {
+				String[] lineSplit = line.split(" ");
+				for(String lexeme: lineSplit) {
+					if(lexeme != "") {
+						lexemes.add(lexeme);
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+			System.out.println("Error: Unable to find file with path: " + pathToFile.toString());
+			System.exit(1);
+		}
+		return lexemes;
+	}
+	class Lexer {
+		public static List<Integer> parseTokens(List<String> lexemes) {
+			/*
+			 * Checks if the lexeme list contains valid tokens.
+			*/
+			assert lexemes.size() != 0;
+			List<Integer> tokens = new ArrayList<Integer>();
+			for(String lexeme: lexemes) {
+				//Check if the lexeme is in the HashMap
+				System.out.println("Validating lexeme: " + lexeme);
+				if(tokenValues.get(lexeme) != null) {
+					tokens.add(tokenValues.get(lexeme));
+				}
+				//Check if the lexeme is an integer literal or identifier
+				else {
+					if(Character.isDigit(lexeme.charAt(0))){
+						//Check if byte identifier is valid and if a value is provided
+						char posZero = lexeme.charAt(0);
+						if(lexeme.length() <= 2) {
+							System.out.println("Error: Unable to identify type of integer due to invalid structure being too short.");
+							System.exit(0);
+						}
+						if(lexeme.charAt(1) == '_' && lexeme.length() <= 2) {
+							System.out.println("Error: Unable to identify type of integer due to invalid structure not clarifying valid byte size.");
+							System.exit(0);
+						}
+						switch(posZero) {
+							//Check if number following type identifier is valid
+							case '1':
+								for(int i = 2; i < lexeme.length(); i++) {
+									if(!Character.isDigit(lexeme.charAt(i))) {
+										System.out.println("Error: Letter character was provided where an integer character was expected");
+										System.exit(1);
+									}
+								}
+								tokens.add(tokenValues.get("int_one_byte"));
+								break;
+							case '2':
+								for(int i = 2; i < lexeme.length(); i++) {
+									if(!Character.isDigit(lexeme.charAt(i))) {
+										System.out.println("Error: Letter character was provided where an integer character was expected");
+										System.exit(1);
+									}
+								}
+								tokens.add(tokenValues.get("int_two_byte"));
+								break;
+							case '4':
+								for(int i = 2; i < lexeme.length(); i++) {
+									if(!Character.isDigit(lexeme.charAt(i))) {
+										System.out.println("Error: Letter character was provided where an integer character was expected");
+										System.exit(1);
+									}
+								}
+								tokens.add(tokenValues.get("int_four_byte"));
+								break;
+							case '8':
+								for(int i = 2; i < lexeme.length(); i++) {
+									if(!Character.isDigit(lexeme.charAt(i))) {
+										System.out.println("Error: Letter character was provided where an integer character was expected");
+										System.exit(1);
+									}
+								}
+								tokens.add(tokenValues.get("int_eight_byte"));
+								break;
+							default:
+								System.out.println("Error: Invalid byte size provided for integer.");
+								System.exit(1);
+								break;
+						}
+					}
+					else if(Character.isLetter(lexeme.charAt(0)) || lexeme.charAt(0) == '_') {
+						//Check if identifier length is correct
+						if(lexeme.length() < 6 || lexeme.length() > 8) {
+							System.out.println("Error: Invalid length provided for identifier.");
+							System.exit(0);
+						}
+						//Check if all characters are letters
+						for(int i = 1; i < lexeme.length(); i++) {
+							if(!(Character.isLetter(lexeme.charAt(i)) || lexeme.charAt(i) == '_')) {
+								System.out.println("Error: A non-letter was provided where a numerical value was expected.");
+								System.exit(1);
+							}
+						}
+						tokens.add(tokenValues.get("id"));
+					}
+					else {
+						System.out.println("Error: Unable to identify token based on starting character.");
+						System.exit(1);
+					}
+				}
+			}
+			return tokens;
+		}
+	}
+}
---
+++ RDA.java
@@ -0,0 +1,373 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class RDA {
+	private int currentToken;
+	private HashMap tokenValues;
+	private List<Integer> tokens;
+	private List<String> lexemes;
+	public RDA(List<String> lexemes, List<Integer> tokens) {
+		this.tokens = tokens;
+		this.lexemes = lexemes;
+		this.tokenValues = Main.tokenValues;
+		this.currentToken = 0;
+	}
+	public void nextToken() {
+		this.currentToken += 1;
+		System.out.println("Next token: " + getCurrentTokenName() + " (" + getCurrentToken() + ")");
+	}
+	public Integer getCurrentToken() {
+		return this.tokens.get(currentToken);
+	}
+	public String getCurrentTokenName() {
+		return this.lexemes.get(currentToken);
+	}
+	public void program() {
+		/*
+		 * <Program> --> BEGIN { <stmt_list> } END 
+		 */
+		System.out.println("Entering <Program>");
+		if(tokenValues.get("BEGIN").equals(getCurrentToken())) {
+			nextToken();
+			if(tokenValues.get("{").equals(getCurrentToken())) {
+				nextToken();
+				stmt_list();
+			}
+			else {
+				System.out.println("Error: No { provided to organize statement list");
+				System.exit(1);
+			}
+			if(tokenValues.get("}").equals(this.tokens.get(currentToken))) {
+				nextToken();
+				//Handle program's end
+				if(tokenValues.get("END").equals(this.tokens.get(currentToken))) {
+					System.out.println("Program analysis has been completed and found no syntax errors.");
+				}
+				else {
+					System.out.println("Error: Program ends with invalid end token");
+					System.exit(1);
+				}	
+			}
+			else {
+				System.out.println("Error: No } provided to organize statement list");
+				System.exit(1);
+			}
+		}
+		else {
+			System.out.print("Error: Program begins with invalid start token.");
+			System.exit(1);
+		}
+		
+	}
+	public void stmt() {
+		/*
+		 * <stmt> --> <when_stmt> | <declr_stmt> | <initialize_stmt> | <loop_stmt>
+		 */
+		System.out.println("Entering <stmt>");
+		Integer token = getCurrentToken();
+		if(tokenValues.get("when") == token) {
+			nextToken();
+			when();
+		}
+		else if(tokenValues.get("loop") == token) {
+			nextToken();
+			loop();
+		}
+		else if(tokenValues.get("num") == token) {
+			nextToken();
+			declr();
+		}
+		else {
+			nextToken();
+			init();
+		}
+	}
+	public void stmt_list() {
+		/*
+		 * <stmt_list> --> <stmt> <stmt_list*>
+		 * <stmt_list*> --> EMPTY | <stmt> <stmt_list*>
+		 */
+		System.out.println("Entering <stmt_list>");
+		if(getCurrentToken() == tokenValues.get("when") || getCurrentToken() == tokenValues.get("num") || getCurrentToken() == tokenValues.get("loop") || getCurrentToken() == tokenValues.get("id")) {
+			stmt();
+			while(getCurrentToken() == tokenValues.get("when") || getCurrentToken() == tokenValues.get("num") || getCurrentToken() == tokenValues.get("loop") || getCurrentToken() == tokenValues.get("id")) {
+				stmt();
+			}
+		}
+	}
+	public void when() {
+		/*
+		 * <if_stmt> --> when ( <bexpr> ) { <stmt_list)> } | when ( <bexpr> ) { <stmt_list> } elsewhen { <stmt_list> }
+		 */
+		System.out.println("Entering <if_stmt>");
+		if(tokenValues.get("(") == getCurrentToken()) {
+			nextToken();
+			bexpr();
+			if(tokenValues.get(")") == getCurrentToken()) {
+				nextToken();
+				if(tokenValues.get("{") == getCurrentToken()) {
+					nextToken();
+					stmt_list();
+					if(tokenValues.get("}") == getCurrentToken()) {
+						nextToken();
+						if(tokenValues.get("elsewhen") == getCurrentToken()) {
+							nextToken();
+							if(tokenValues.get("{") == getCurrentToken()) {
+								nextToken();
+								stmt_list();
+								if(tokenValues.get("}") == getCurrentToken()) {
+									nextToken();
+								}
+								else {
+									System.out.println("Error: Missing } character in elsewhen statement");
+									System.exit(1);
+								}
+							}
+							else {
+								System.out.println("Error: Missing { character in elsewhen statement");
+								System.exit(1);
+							}
+							stmt_list();
+						}
+					}
+					else {
+						System.out.println("Error: Missing } character in when statement");
+						System.exit(1);
+					}
+				}
+				else {
+					System.out.println("Error: Missing { character in when statement");
+					System.exit(1);
+				}
+			}
+			else {
+				System.out.println("Error: Missing ) character in when statement");
+				System.exit(1);
+			}
+		}
+		else {
+			System.out.println("Error: Missing ( character in when statement");
+			System.exit(1);
+		}
+	}
+	public void loop() {
+		/*
+		 * <loop_stmt> --> loop ( <expr> ) { <stmt_list> }
+		 */
+		System.out.println("Entering <loop_stmt>");
+		if(tokenValues.get("(") == getCurrentToken()) {
+			nextToken();
+			bexpr();
+			if(tokenValues.get(")") == getCurrentToken()) {
+				nextToken();
+				if(tokenValues.get("{") == getCurrentToken()) {
+					nextToken();
+					stmt_list();
+					if(tokenValues.get("}") == getCurrentToken()) {
+						nextToken();
+					}
+					else {
+						System.out.println("Error: Missing } character in loop statement");
+						System.exit(1);
+					}
+				}
+				else {
+					System.out.println("Error: Missing { character in loop statement");
+					System.exit(1);
+				}
+			}
+			else {
+				System.out.println("Error: Missing ) character in loop statement");
+				System.exit(1);
+			}
+		}
+		else {
+			System.out.println("Error: Missing ( character in loop statement");
+			System.exit(1);
+		}
+	}
+	public void declr() {
+		/*
+		 * <declr> --> num id
+		 */
+		System.out.println("Entering <declr>");
+		if(tokenValues.get("id") == getCurrentToken()) {
+			nextToken();
+		}
+		else {
+			System.out.println("Error: Missing an identifier in declaration statement.");
+			System.exit(1);
+		}
+	}
+	public void init() {
+		/*
+		 * <assign> --> id = <expr>
+		 */
+		System.out.println("Entering <init>");
+		if(tokenValues.get("=") == getCurrentToken()) {
+			nextToken();
+			expr();
+		}
+		else {
+			System.out.println("Error: Missing an = character in initialization statement.");
+			System.exit(1);
+		}
+	}
+	public void expr() {
+		/*
+		 *<expr> -> <mul_op> <add_op*>
+		 * <expr*> -> EMPTY | + <mul_op> <add_op*> | <mul_op> 
+		 */
+		System.out.println("Entering <expr>");
+		mul();
+		while(tokenValues.get("+") == getCurrentToken()) {
+			nextToken();
+			mul();	
+		}
+	}
+	public void mul() {
+		/*
+		 * <mul_op> -> <sub_op> <mul_op*>
+		 * <mul_op*> -> EMPTY | * <sub_op> <mul_op*> | <sub_op>
+		 */
+		System.out.println("Entering <mul_op>");
+		sub();
+		while(tokenValues.get("*") == getCurrentToken()) {
+			nextToken();
+			sub();	
+		}
+	}
+	public void div() {
+		/*
+		 * <div_op> -> <mod_op> <div_op*>
+		 * <div_op*> -> EMPTY | / <mod_op> <div_op*> | <mod_op>
+		 */
+		System.out.println("Entering <div_op>");
+		mod();
+		while(tokenValues.get("/") == getCurrentToken()) {
+			nextToken();
+			mod();	
+		}
+	}
+	public void sub() {
+		/*
+		 * <sub_op> -> <div_op> <sub_op*>
+		 * <sub_op*> -> EMPTY | - <div_op> <sub_op*> | <div_op>
+		 */
+		System.out.println("Entering <sub_op>");
+		div();
+		while(tokenValues.get("-") == getCurrentToken()) {
+			nextToken();
+			div();
+		}
+	}
+	public void mod() {
+		/*
+		 * <mod_op> -> <exp_op> <mod_op*>
+		 * <mod_op*> -> EMPTY | % <exp_op> <mod_op> | <exp_op>
+		 */
+		System.out.println("Entering <mod_op>");
+		exp();
+		while(tokenValues.get("%") == getCurrentToken()) {
+			nextToken();
+			exp();
+		}
+	}
+	public void exp() {
+		/*
+		 * <exp_op> -> <term> <exp_op*>
+		 * <exp_op> -> EMPTY | E <term> <exp_op*> | <term>
+		 */
+		System.out.println("Entering <exp_op>");
+		term();
+		while(tokenValues.get("E") == getCurrentToken()) {
+			nextToken();
+			term();
+		}
+	}
+	public void term() {
+		/*
+		 * <term> -> id | integer | ( <expr> )
+		 */
+		System.out.println("Entering <term>");
+		if(tokenValues.get("int_one_byte") == getCurrentToken() || tokenValues.get("int_two_byte") == getCurrentToken() || tokenValues.get("int_four_byte") == getCurrentToken() || tokenValues.get("int_eight_byte") == getCurrentToken() || tokenValues.get("id") == getCurrentToken()) {
+			nextToken();
+		}
+		else if(tokenValues.get("(") == getCurrentToken()) {
+			nextToken();
+			expr();
+			if(tokenValues.get(")") == getCurrentToken()) {
+				nextToken();
+			}
+			else {
+				System.out.println("Error: Missing closing ) in expression");
+				System.exit(1);
+			}
+		}
+		else {
+			System.out.println("Error: Invalid token found in expression statement");
+			System.exit(1);
+		}
+	}
+	public void bexpr() {
+		/*
+		 * <bexpr> --> <bor> <bexpr*>
+		 * <bexpr*> --> EMPTY | & <bexpr*> | <bor>
+		 */
+		System.out.println("Entering <bexpr>");
+		bor();
+		while(tokenValues.get("&") == getCurrentToken()) {
+			nextToken();
+			bor();
+		}
+	}
+	public void bor() {
+		/*
+		 *<bor> --> <beq> <bor*>
+		 *<bor*> --> EMPTY | || <bor*> | <beq>
+		 */
+		System.out.println("Entering <bor>");
+		beq();
+		while(tokenValues.get("|") == getCurrentToken()) {
+			nextToken();
+			beq();
+		}
+	}
+	public void beq() {
+		/*
+		 * <beq> --> <bcomp> <beq*>
+		 * <beq*> --> EMPTY | == <beq*> | != <beq*> | <bcomp>
+		 */
+		System.out.println("Entering <beq>");
+		bcomp();
+		while(tokenValues.get("==") == getCurrentToken() || tokenValues.get("!=") == getCurrentToken()) {
+			nextToken();
+			bcomp();
+		}
+	}
+	public void bcomp() {
+		/*
+		 * <bcomp> --> <bexpr> <bcomp*>
+		 * <bcomp*> --> EMPTY | > <bcomp*> | < <bcomp*> | >= <bcomp*> | <= <bcomp*> | <bexpr>
+		 */
+		System.out.println("Entering <bcomp>");
+		bnot();
+		while(tokenValues.get(">") == getCurrentToken() || tokenValues.get("<") == getCurrentToken() || tokenValues.get(">=") == getCurrentToken() || tokenValues.get("<=") == getCurrentToken()) {
+			nextToken();
+			bnot();
+		}
+	}
+	public void bnot() {
+		/*
+		 *  <bnot> --> !<expr> | <expr>
+		 */
+		System.out.println("Entering <bnot>");
+		if(tokenValues.get("!") == getCurrentToken()) {
+			nextToken();
+			expr();
+		}
+		else {
+			expr();
+		}
+	}
+}
